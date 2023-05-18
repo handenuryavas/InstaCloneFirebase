453194a13f196274e9270c277db0733399ad71ea
handenuryavas <hande.nur038@gmail.com>
2023-05-18 17:05:15 +0300

    Instagram Clone

---
+++ InstaCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Hande Nur Yavaş on 17.05.2023.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logOutClicked(_ sender: Any)
+    {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        } catch {
+            print("error")
+        }
+       
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstaCloneFirebase/TabController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+public class TabController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
